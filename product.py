fd909d9f831f29acbe2148a2de65fd18e1977e4f
Tank Chen <a9304107@gmail.com>
2022-03-07 18:20:03 +0800

    新增程式:添加菜單，與詢問內容及建立csv檔

---
+++ product.py
@@ -0,0 +1,33 @@
+products = []
+m = 0
+print('建立菜單嚕~')
+while True:
+	name = input('請輸入餐點：(q=離開)')
+	if name == 'q':
+		break
+	price = input('請輸入價格')
+	products.append([name, price])
+print(products)
+while True:
+	q = input('您有要詢問的嗎?餐點/價格/q(離開)')
+	if q == '餐點':
+		n = input('您要詢問什麼餐點:')
+		for a in products:
+			if n == a[0]:
+				print(n, '的價格為', a[1])
+				m += 1
+		if m == 0:
+			print('沒有賣這餐點喔~')
+	elif q == '價格':
+		n = input('您要詢問哪個價位:')
+		for a in products:
+			if n == a[1]:
+				print(n, '價格的餐點有', a[0])
+				m += 1
+		if m == 0:
+			print('沒有這價位的餐點喔~')					
+	else:
+		break
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
